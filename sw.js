4ff4212b1b4cc0b0235e56d00ad5fd99ae59b55d
unknown <zafrn@example.com>
2026-04-16 20:47:54 +0800

    Site updated: 2026-04-16 20:47:53

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1776343273676";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1776343652552";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
