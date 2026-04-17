09f4919e5b1d41d3693652dcae2084d173477b2b
unknown <zafrn@example.com>
2026-04-17 18:40:33 +0800

    Site updated: 2026-04-17 18:40:32

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1776420361791";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1776422419940";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
